2f2b955fb9de366ad24e7cf411a3b02a1d535a6b
Nathan Andrade <nathan.nhdsk@gmail.com>
2025-11-07 00:14:03 -0300

    refactor: remove validação

--- src/main/java/com/github/andradenathan/Arguments.java
+++ src/main/java/com/github/andradenathan/Arguments.java
@@ -6,22 +6,4 @@ public record Arguments(
   public static Arguments usingDefault() {
     return new Arguments(9.81, 10.0, 150.0, 10.0, 150.0);
   }
-
-  private void validate(double gravity, double m1, double m2, double l1, double l2) {
-    if (gravity <= 0) {
-      throw new IllegalArgumentException("Gravity must be positive.");
-    }
-    if (m1 <= 0 || m2 <= 0) {
-      throw new IllegalArgumentException("Masses must be positive.");
-    }
-    if (l1 <= 0 || l2 <= 0) {
-      throw new IllegalArgumentException("Lengths must be positive.");
-    }
-  }
-
-  @Override
-  public String toString() {
-    return String.format(
-        "g=%.2f, m1=%.2f, m2=%.2f, L1=%.2f, L2=%.2f", gravity, mass1, length1, mass2, length2);
-  }
 }
